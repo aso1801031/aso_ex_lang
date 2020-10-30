ce03612333177dc105c8533a5ff7813da3dc19e9
aso1801008 <aso1801008@s.asojuku.ac.jp>
2020-10-30 12:34:50 +0900

    江崎1030

---
+++ pages/user-detail.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-row>
+    <v-col class="text-center info">
+      <img
+        src=""
+        alt="noImage"
+        class="mb-5"
+      >
+    <h1>{{name}}</h1>
+    <div id = "detail">
+        <p>Language : {{ Language }}</p>
+        <hr>
+        <p>Birth : {{ Birth }}</p>
+        <hr>
+        <p>profile : </p>
+        <hr>
+        <p>{{ profile }}</p>
+    </div>
+    
+    </v-col>
+  </v-row>
+</template>
+<script>
+    export default {
+        data: function(){
+            return{
+                name:"Ezaki Hiroshi",
+                Language:"japanese",
+                Birth:"1999/05/10",
+                profile:"Hello!"
+            };
+        },
+    };
+</script>
+
+<router>
+  path: '/profile'
+</router>
+
+<style>
+#detail {
+    border-radius: 10px;
+    background-color: white;
+    padding:15px;
+}
+hr{
+    width:80%;
+    margin-left:10%;
+}
+p{
+    margin-top:15px;
+}
+</style>
