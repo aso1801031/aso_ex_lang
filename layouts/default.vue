fd402d16f89a6975b469046a506bf4086a849873
aso1901103 <1901103@s.asojuku.ac.jp>
2020-10-30 11:46:40 +0900

    layoutの更新

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,84 +1,36 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
+  <v-app light>
+    <v-app-bar
+      :clipped-left="clipped"
       fixed
       app
+      class="secondary white--text"
     >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
+     <h1>
+      <nuxt-link to='/index' class="white--text" style="text-decoration: none;">
+       Service Name
+      </nuxt-link>
+     </h1>
+      <v-spacer />
+      <div v-for="(item,i) in items"
           :key="i"
           :to="item.to"
           router
           exact
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+          <nuxt-link v-bind='item' class="white--text mr-3" style="text-decoration: none;">
+            {{item.title}}
+          </nuxt-link>
+
+      </div>
+
+
     </v-app-bar>
-    <v-main>
+    <v-main class="info">
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
       app
@@ -98,14 +50,30 @@ export default {
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          title: '検索',
+          to: '/search'
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: '予定確認',
+          to: '/schedule'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: '学習履歴確認',
+          to: '/history'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'レッスン登録',
+          to: '/lesson_register'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'プロフィール',
+          to: '/profile'
         }
+
       ],
       miniVariant: false,
       right: true,
---
+++ layouts/default2.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app light>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      class="secondary white--text"
+    >
+     <h1>
+      <nuxt-link to='/' class="white--text" style="text-decoration: none;">
+       Service Name
+      </nuxt-link>
+     </h1>
+
+
+    </v-app-bar>
+    <v-main class="info">
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
