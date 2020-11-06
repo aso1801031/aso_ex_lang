3bcc467cca062fa6c08b74642bd79555282ba3e1
aso1801031 <1801031@s.asojuku.ac.jp>
2020-11-06 10:48:51 +0900

    カレンダー修正

---
+++ pages/search.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h3 class="search_title">search lesson!</h3>
+        <v-container>
+
+            <v-row class="mt-5">
+            <v-col cols="12" md="6" class="element_left">
+                column 1
+            </v-col>
+            <v-col cols="12" md="6" class="element_right">
+                column 2
+            </v-col>
+            </v-row>
+            
+            <v-row class="mt-5">
+                <v-col cols="12" md="6" class="element_left">
+                    column 1
+                </v-col>
+                <v-col cols="12" md="6" class="search_right">
+                    column 2
+                </v-col>
+            </v-row>
+    
+        </v-container>
+    </div>
+</template>
+<style>
+    .search_title{
+        text-align: center;
+    }
+</style>
+  
