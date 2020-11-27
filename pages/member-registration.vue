1432de87065771544ec0e3b812b7f04af47a766d
ASO1810006 <1801006@s.asojuku.ac.jp>
2020-11-27 16:07:02 +0900

    dahaaaaaaaaaaaaaaaa

--- pages/member-registration.vue
+++ pages/member-registration.vue
@@ -32,7 +32,7 @@
                             <h3 class="mt-10">Password</h3>
                         </v-col>
                         <v-col cols="12" md="8">
-                        <ValidationProvider name="password" v-slot="{ errors }" rules="required|max:50" vid="confirmation">
+                        <ValidationProvider name="password" v-slot="{ errors }" rules="required|max:50|min:5" vid="confirmation">
                             <v-text-field
                             v-model="confirmation"
                             label="password"
