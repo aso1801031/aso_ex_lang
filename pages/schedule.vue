cd1cef82b618bd46e0b90b436c250b5cd5672426
aso1801031 <1801031@s.asojuku.ac.jp>
2020-12-10 15:58:43 +0900

    スケジュール画面編集

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -7,15 +7,35 @@
     elevation="3"
     :weekday-format="getDay"
     :allowed-dates='allowedDate'
-    :events="arrayEvents"
-    event-color="pink lighten-1"
+    :events="functionEvents"
     v-model="picker"
     />
     <p class="mt-1 mb-2 today">{{l_date}}</p>
-    <div v-for="allLesson of allLessons" :key="allLesson.title" v-if="allLesson.lesson_date == l_date">
-      <p class="lesson_time">{{ allLesson.lesson_time }}</p>
-      <a class="title_link" v-bind:href="getLessonDate(allLesson.lesson_time)"><h5 class="lesson_title">{{allLesson.title}}</h5></a>
+    <!--教師側予定表示-->
+    <p class="teacher_text mb-0 pb-0" v-if="teachTextFlg(l_date)">Teacher</p>
+    <div v-for="allLesson of teachAllLessons" :key="allLesson.data.title" v-if="allLesson.data.lesson_date == l_date">
+      <v-row>
+        <v-col cols="12"  md="1">
+          <p class="lesson_time">{{ allLesson.data.lesson_time }}</p>
+        </v-col>
+        <v-col cols="12"  md="11">
+          <a class="title_link" v-bind:href="allLesson.id"><h5 class="lesson_title">{{allLesson.data.title}}</h5></a>
+        </v-col>
+      </v-row>
     </div>
+    <!--生徒側予定表示-->
+    <p class="student_text mb-0 pb-0" v-if="stuTextFlg(l_date)">Student</p>
+    <div v-for="allLesson of studentAllLessons" :key="allLesson.data.title" v-if="allLesson.data.lesson_date == l_date">
+      <v-row>
+        <v-col cols="12"  md="1">
+          <p class="lesson_time">{{ allLesson.data.lesson_time }}</p>
+        </v-col>
+        <v-col cols="12"  md="9">
+          <a class="title_link_s" v-bind:href="allLesson.id"><h5 class="lesson_title">{{allLesson.data.title}}</h5></a>
+        </v-col>
+      </v-row>
+    </div>
+
 </div>
 </template>
 
@@ -27,9 +47,10 @@ export default {
     return {
       url:"",
       l_date: null,
-      allLessons: [],
-      lessonIds:[],
-      arrayEvents: null,
+      teachAllLessons: [],
+      studentAllLessons:[],
+      teachEvents:[],
+      studentEvents:[],
     };
   },
   computed: {
@@ -47,17 +68,6 @@ export default {
       const daysOfWeek = ['Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
       let i = new Date(date).getDay(date)
       return daysOfWeek[i]
-    },
-    getLessonDate(l_time){
-      this.url="/schedule-detail/"
-      for(var i = 0; i < this.allLessons.length; i++){
-        //console.log(this.allLessons[i].lesson_date,this.l_date)
-        if(this.allLessons[i].lesson_date === this.l_date && 
-        this.allLessons[i].lesson_time === l_time){
-          return this.url+this.lessonIds[i]
-        }
-      }
-      return
     },
     allowedDate:function(val){
       let today = new Date()
@@ -78,38 +88,60 @@ export default {
       )
       return today <= new Date(val)
     },
+    teachTextFlg(date){
+      let t_textflg = false;
+      this.teachAllLessons.forEach( lesson => {
+        if(lesson.data.lesson_date == date){
+          t_textflg = true
+        }
+      })
+      return t_textflg
+    },
+    stuTextFlg(date){
+      let s_textflg = false;
+      this.studentAllLessons.forEach( lesson => {
+        if(lesson.data.lesson_date == date){
+          s_textflg = true
+        }
+      })
+      return s_textflg
+    },
+    functionEvents(date){
+      if(this.teachEvents.includes(date) && this.studentEvents.includes(date)){
+        return ["pink","blue"]
+      }else if(this.teachEvents.includes(date)){
+        return ["pink"]
+      } else if(this.studentEvents.includes(date)){ 
+        return ["blue"]
+      }
+      return false
+    },
   },
   created:function () {
-    const events=[];
     // 現在の時間を取得
     let today = new Date();
     let l_date;
-    firebase.firestore().collection('lessons').get().then(snapshot => {
+    firebase.firestore().collection('lessonAttendances').get().then(snapshot => {
       snapshot.forEach(doc => {
-        l_date = new Date(doc.data().lesson_date+" "+doc.data().lesson_time)
+        //console.log("id=",doc.data().student_id.id)
+        doc.data().lesson_id.get().then(res => { 
+          //console.log("res id=",res.id,"store=",this.$store.state.id,res.id === this.$store.state.id)
+          l_date = new Date(res.data().lesson_date+" "+res.data().lesson_time)
+          //console.log(l_date)
+          //console.log(res.data())
+          if(doc.data().teacher_id.id === this.$store.state.id && today < l_date){
+            this.teachAllLessons.push({id:"/schedule-detail/"+res.id,data:res.data()})
+            this.teachEvents.push(res.data().lesson_date)
+          }else if(doc.data().student_id.id === this.$store.state.id && today < l_date){
+            this.studentAllLessons.push({id:"/choose_lesson/"+res.id,data:res.data()})
+            this.studentEvents.push(res.data().lesson_date)
+          }
 
-        if(doc.data().teacher_id.id === this.$store.state.id && today < l_date){
-          this.allLessons.push(doc.data())
-          this.lessonIds.push(doc.id)
-          events.push(doc.data().lesson_date)
-          //console.log(events,events.length,this.allLessons[0].title)
-          let cnt = 0;
-          this.arrayEvents = [...Array(events.length)].map(() => {
-              const day = new Date(events[cnt]).getDate()
-              const d = new Date()
-              d.setDate(day)
-              //console.log("day",day,"d",d.toISOString().substr(0, 10))
-              cnt++
-              return d.toISOString().substr(0, 10)
-          })
-
-        }
-
+        })
       })
     })
     console.log(this.$store.state.id)
-    console.log("created")
-
+    //console.log("created")
   },
 
 };
@@ -155,24 +187,39 @@ export default {
   }
   .lesson_time{
     font-size: 1.2em;
-    margin-bottom: 0%;
-    margin-top: 10px;
     color: dimgray;
+    margin: 0px;
   }
   .lesson_title{
     font-size: 1.2em;
-    margin: 0px;
     display:inline;
+  }
+  .teacher_text{
+    font-size: 1.2em;
+    color: crimson;
+  }
+  .student_text{
+    font-size: 1.2em;
+    color: dodgerblue;
   }
   a.title_link{
   transition: color 0.3s;
   text-decoration: none;
+  margin-left: 8px;
+  }
+  a.title_link_s{
+  transition: color 0.3s;
+  text-decoration: none;
+  margin-left: 8px;
   }
   .today{
     font-size: 1.25em;
   }
   a.title_link:hover{
   color : crimson;
+  }
+  a.title_link_s:hover{
+  color :dodgerblue
   }
   .v-application p {
     margin-bottom: 0px;
